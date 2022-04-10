f235239ab7ff13e5e0233c3fc6b85b3c16407ace
David Arámbula <drakodarpan@gmail.com>
2022-04-09 22:16:13 -0600

    Add template generado - 42

--- apps/libro/forms.py
+++ apps/libro/forms.py
@@ -6,3 +6,36 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = ['nombre', 'apellidos', 'nacionalidad', 'descripcion']
+        labels = {
+            'nombre': 'Nombre del autor: ',
+            'apellidos': 'Apellidos del autor: ',
+            'nacionalidad': 'Nacionalidad del autor: ',
+            'descripcion': 'Pequeña descripción: ',
+        }
+        widgets = {
+            'nombre': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'required': 'true',
+                    'placeholder': 'Ingresa el nombre del autor...',
+                }
+            ),
+            'apellidos': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingresa los apellidos del autor...',
+                }
+            ),
+            'nacionalidad': forms.TextInput(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingresa la nacionalidad del autor...',
+                }
+            ),
+            'descripcion': forms.Textarea(
+                attrs = {
+                    'class': 'form-control',
+                    'placeholder': 'Ingresa la descripción del autor...',
+                }
+            )
+        }
