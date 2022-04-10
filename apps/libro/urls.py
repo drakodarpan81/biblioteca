d8fb41ef93b036caba9acec2c5a014d0dabe80c4
David Arámbula <drakodarpan@gmail.com>
2022-04-09 22:57:29 -0600

    Add all views - 45

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('crear_autor/', crearAutor, name='crear_autor'),
-    path('editar_autor/<int:id>', editarAutor, name='editar_autor'),
-    path('eliminar_autor/<int:id>', eliminarAutor, name='eliminar_autor'),
     path('', InicioView.as_view(), name='index'),
     path('listar_autor/', AutorListView.as_view(), name='listar_autor'),
+    path('editar_autor/<int:pk>', AutorUpdateView.as_view(), name='editar_autor'),
+    path('crear_autor/', AutorCreateView.as_view(), name='crear_autor'),
+    path('eliminar_autor/<int:pk>', AutorDeleteView.as_view(), name='eliminar_autor'),
 ]
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .forms import AutorForm
 from .models import *
 from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class InicioView(TemplateView):
@@ -15,42 +16,29 @@ class AutorListView(ListView):
     queryset = Autor.objects.filter(estado=True)
 
 
-def crearAutor(request):
-    if request.method == 'POST':
-        autor_form = AutorForm(request.POST)
-        if autor_form.is_valid():
-            nom = autor_form.cleaned_data['nombre']
-            print(nom)
-
-            autor_form.save()
-            return render(request, 'index.html')
-    else:
-        autor_form = AutorForm()
-
-    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form})
+class AutorUpdateView(UpdateView):
+    model = Autor
+    template_name = "libro/crear_autor.html"
+    form_class = AutorForm
+    success_url = reverse_lazy('libro:listar_autor')
 
 
-def editarAutor(request, id):
+class AutorCreateView(CreateView):
+    model = Autor
+    template_name = 'libro/crear_autor.html'
+    form_class = AutorForm
+    success_url = reverse_lazy('libro:listar_autor')
 
-    autor_form = None
-    error = None
 
-    try:
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = 'libro/eliminar_autor.html'
 
-        autor = Autor.objects.get(id=id)
-
-        if request.method == 'GET':
-            autor_form = AutorForm(instance=autor)
-        else:
-            autor_form = AutorForm(request.POST, instance=autor)
-            if autor_form.is_valid():
-                autor_form.save()
-            return redirect('libro:listar_autor')
-
-    except ObjectDoesNotExist as e:
-        error = e
-
-    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form, 'error': error})
+    def post(self, request, pk, *args, **kwargs):
+        object = Autor.objects.get(id=pk)
+        object.estado = False
+        object.save()
+        return redirect('libro:listar_autor')
 
 
 def eliminarAutor(request, id):
